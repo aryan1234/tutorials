f171256653fab93b93b4c11a89745ba024481cd1
aryan1234 <kanwararyan1@gmail.com>
2017-02-09 13:07:06 +0530

    Improved readability

--- logout.php
+++ logout.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 session_destroy();
-$username=$_COOKIE['username'];
-$password=$_COOKIE['password'];
+$username = $_COOKIE['username'];
+$password = $_COOKIE['password'];
 
 if(isset($_COOKIE['username']) AND isset($_COOKIE['password'])){
-	setcookie('username',$username,time()-1);
-	setcookie('password',$password,time()-1);
+	setcookie('username', $username,time()-1);
+	setcookie('password', $password,time()-1);
 }
 echo "You logged out";
 echo "Click to login <a href='login.php'>Login</a>";
