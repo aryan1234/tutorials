9632b531fabc356bca095c46a3235403ef807b09
aryan1234 <kanwararyan1@gmail.com>
2017-02-09 13:05:35 +0530

    Improved readability

--- dashboard.php
+++ dashboard.php
@@ -1,17 +1,9 @@
 <?php
 session_start();
-if($_SESSION['username'])
-{
-echo "Welcome to your dashboard".$_SESSION['username'];
-
-echo '<br>';
-echo '<a href="logout.php">Logout</a>';
+if($_SESSION['username']){
+	echo "Welcome to your dashboard".$_SESSION['username'];
+	echo '<br>';
+        echo '<a href="logout.php">Logout</a>';
 }
-else
-{
-	echo "Authentication failed";
-}
-
-
-
+echo "Authentication failed";
 ?>
