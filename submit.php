4f3005045c34db6ff7e7d9989e43b88b831eb92e
aryan1234 <kanwararyan1@gmail.com>
2017-02-09 12:56:20 +0530

    Improved readability

--- submit.php
+++ submit.php
@@ -1,31 +1,18 @@
 <?php
 require('connect.php');
-$username=$_POST['username'];
-$password=$_POST['password'];
+$username = $_POST['username'];
+$password = $_POST['password'];
 
 $query=$handler->query("SELECT * FROM Login WHERE username='$username' AND password='$password'" );
-$r=$query->fetch();
-if($r)
-{
-
+$r = $query->fetch();
+if($r){
 	if(isset($_POST['remeber'])){
 			setcookie('username',$username,time()+60*60*7);
-			setcookie('password',$password,time()+60*60*7);
-			
+			setcookie('password',$password,time()+60*60*7);			
 	}
-     
-   			session_start();
-			$_SESSION['username']=$username;
-			header('location:dashboard.php');
-
+  	session_start();
+	$_SESSION['username'] = $username;
+	header('location:dashboard.php');
 }
-
-else
-{
-	echo "Invalid username or password";
-}
-	
-
-
-
+echo "Invalid username or password";
 ?>
